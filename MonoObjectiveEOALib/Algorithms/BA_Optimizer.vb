ae6c644d1a6d609179d9481255924ab8b7045e9a
SaadDAHMANI <TahatAtakor7@gmail.com>
2020-12-08 19:08:05 +0100

    Add  Inheristance of EvolutionaryAlgoBase.

--- MonoObjectiveEOALib/Algorithms/BA_Optimizer.vb
+++ MonoObjectiveEOALib/Algorithms/BA_Optimizer.vb
@@ -13,5 +13,64 @@
 '' Writen in VB.NET by S.DAHMANI (sd.dahmani2000@gmail.com)
 ''___________________________________________________________________________________________
 Public Class BA_Optimizer
+    Inherits EvolutionaryAlgoBase
+    Public Overrides ReadOnly Property AlgorithmName As Object
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
 
+    Public Overrides ReadOnly Property AlgorithmFullName As Object
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property BestSolution As Double()
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property BestChart As List(Of Double)
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property WorstChart As List(Of Double)
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property MeanChart As List(Of Double)
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Solution_Fitness As Dictionary(Of String, Double)
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property CurrentBestFitness As Double
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public Overrides Sub RunEpoch()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub InitializeOptimizer()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub ComputeObjectiveFunction(positions() As Double, ByRef fitnessValue As Double)
+        Throw New NotImplementedException()
+    End Sub
 End Class
